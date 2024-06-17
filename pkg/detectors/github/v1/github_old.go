8dc1a7aa0fbe19a9f7bb78b8d1064465ba9bafbc
Richard Gomez <rmgomez368@gmail.com>
2024-06-17 19:36:50 -0400

    custom(github): update keywords

--- pkg/detectors/github/v1/github_old.go
+++ pkg/detectors/github/v1/github_old.go
@@ -26,7 +26,7 @@ func (Scanner) DefaultEndpoint() string { return "https://api.github.com" }
 var (
 	// Oauth token
 	// https://developer.github.com/v3/#oauth2-token-sent-in-a-header
-	keyPat = regexp.MustCompile(`(?i)(?:github|gh|pat|token)[^\.].{0,40}[ =:'"]+([a-f0-9]{40})\b`)
+	keyPat = regexp.MustCompile(`(?i)(?:github|gh|hub|pat|token|octo)[^\.].{0,40}[ =:'"]+([a-f0-9]{40})\b`)
 
 	// TODO: Oauth2 client_id and client_secret
 	// https://developer.github.com/v3/#oauth2-keysecret
@@ -54,7 +54,7 @@ type HeaderInfo struct {
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"github", "gh", "pat", "token"}
+	return []string{"github", "gh", "hub", "pat", "token", "octo"}
 }
 
 // FromData will find and optionally verify GitHub secrets in a given set of bytes.
