c19eda3588148d32c54f7f5584e095031848d6f0
Richard Gomez <rmgomez368@gmail.com>
2024-12-28 16:38:49 -0500

    feat(privatekey): warn if new logic missed match

--- pkg/detectors/privatekey/normalize.go
+++ pkg/detectors/privatekey/normalize.go
@@ -160,3 +160,29 @@ func getContentFooterLine(data []byte) string {
 	}
 	return ""
 }
+
+func normalizeOld(in string) string {
+	in = strings.ReplaceAll(in, `"`, "")
+	in = strings.ReplaceAll(in, `'`, "")
+	in = strings.ReplaceAll(in, "\t", "")
+	in = strings.ReplaceAll(in, `\t`, "")
+	in = strings.ReplaceAll(in, `\\t`, "")
+	in = strings.ReplaceAll(in, `\n`, "\n")
+	in = strings.ReplaceAll(in, `\\r\\n`, "\n")
+	in = strings.ReplaceAll(in, `\r\n`, "\n")
+	in = strings.ReplaceAll(in, "\r\n", "\n")
+	in = strings.ReplaceAll(in, `\\r`, "\n")
+	in = strings.ReplaceAll(in, "\r", "\n")
+	in = strings.ReplaceAll(in, `\r`, "\n")
+	in = strings.ReplaceAll(in, `\\n`, "\n")
+	in = strings.ReplaceAll(in, `\n\n`, "\n")
+	in = strings.ReplaceAll(in, "\n\n", "\n")
+	in = strings.ReplaceAll(in, `\\`, "\n")
+
+	cleaned := strings.Builder{}
+	parts := strings.Split(in, "\n")
+	for _, line := range parts {
+		cleaned.WriteString(strings.TrimSpace(line) + "\n")
+	}
+	return cleaned.String()
+}
